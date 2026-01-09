7c8ca3fd75b45ece88f7c189dde48ac59fedfd59
Daniel Worth <pipemanmusic@gmail.com>
2026-01-09 09:26:57 -0700

    Implement DeviceWizard dialog for PH5-DEV.1; requires name for validation. Test passes.

---
+++ ui/dialogs/device_wizard.py
@@ -0,0 +1,25 @@
+from PySide6.QtWidgets import QDialog, QVBoxLayout, QLineEdit, QPushButton, QLabel
+
+class DeviceWizard(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("Device Wizard")
+        layout = QVBoxLayout(self)
+
+        self.name_label = QLabel("Device Name:")
+        layout.addWidget(self.name_label)
+        self.name_input = QLineEdit()
+        layout.addWidget(self.name_input)
+
+        self.accept_button = QPushButton("Create")
+        self.accept_button.setEnabled(False)
+        layout.addWidget(self.accept_button)
+
+        self.name_input.textChanged.connect(self._on_name_changed)
+        self.accept_button.clicked.connect(self.accept)
+
+    def _on_name_changed(self, text):
+        self.accept_button.setEnabled(self.validate())
+
+    def validate(self):
+        return bool(self.name_input.text().strip())
